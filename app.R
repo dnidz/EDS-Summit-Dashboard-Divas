0faad6edcefb00d316bf4a8d37a9577dc9c9cd97
Caitlin Mothes <ccmothes@gmail.com>
2026-01-28 13:12:35 -0800

    initial repo set up

---
+++ app.R
@@ -0,0 +1,21 @@
+# Dashboard showing Pilot survey results from EDS Summit 2026
+# Survey topic = How to define success and actionable outcomes in community-based science
+
+# SETUP -------------------
+library(shiny)
+librray(shinydashboard)
+
+# UI ---------------------
+ui <- fluidPage(
+
+
+)
+
+# SERVER -------------------
+server <- function(input, output) {
+
+   
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
